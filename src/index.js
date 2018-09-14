a83d0b1069a4b3063a33bf4983e5e1018c196091
montes5 <alexmontes5@yandex.ru>
2018-09-14 19:08:39 +0300

    solution1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    var solution = 0;
+    while(number > 0)
+    {
+        number = Math.floor(number/5);
+        solution += number;
+    }
+    return solution;
 }
